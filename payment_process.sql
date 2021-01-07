e8e9cdcd360117759e0ab10a3e10b71d6811be95
SAYAKGHANTA <44060681+SAYAKGHANTA@users.noreply.github.com>
2021-01-08 00:18:41 +0530

    Update payment_process.sql

--- payment_process.sql
+++ payment_process.sql
@@ -5,17 +5,19 @@ set serveroutput on
 -----------------------------------------------------------------------------------------------------------
 create table PaymentDetails(
                     UserID          NUMBER      NOT NULL,  
-                    CardNumber      NUMBER      NOT NULL,
-                    CVV_Number      NUMBER      NOT NULL,
+                    CardNumber      VARCHAR2(20)     NOT NULL,
+                    CVV_Number      VARCHAR2(20)    NOT NULL,
                     Time            DATE,
                     TransactionID       NUMBER 
 );
 
+/*
 CREATE SEQUENCE pay_id
 MINVALUE 1
 START WITH 12350012
 INCREMENT BY 1
 CACHE 10;
+*/
 
 ACCEPT courseid PROMPT 'Enter courseid : '
 ACCEPT I PROMPT 'Enter your userid:'
@@ -24,8 +26,8 @@ ACCEPT cvv PROMPT 'Enter the cvv number of you card :'
 
 DECLARE               
             UserID          NUMBER  ;
-            CardNumber      NUMBER ;   
-            CVV_Number      NUMBER  ;
+            CardNumber      VARCHAR2(20) ;   
+            CVV_Number     VARCHAR2(20)  ;
             Time            DATE;
             pId             number;
             id       VARCHAR2(20);
@@ -35,25 +37,27 @@ DECLARE
 BEGIN
     
     UserID:=&I;
-    CardNumber :=&N;
-    CVV_Number :=&cvv;
+    CardNumber :='&N';
+    CVV_Number :='&cvv';
     id:='&courseid';
-    
-    SELECT fees INTO amount FROM course WHERE courseid = id;
-    SELECT coursename INTO name FROM course WHERE courseid = id;
+    SELECT fees INTO amount 
+    FROM course
+    WHERE courseid = id;
+    SELECT coursename INTO name 
+    FROM course
+    WHERE courseid = id;
     SELECT TotalEnrolled INTO seat FROM course  WHERE courseid = id;
-    
     DECLARE
     card_isvalid   BOOLEAN;
     BEGIN
-       card_isvalid := REGEXP_LIKE (CardNUmber, '^[[:digit:]]+$');
+       card_isvalid := REGEXP_LIKE (CardNUmber, '^[0-9]+$');
 
        IF card_isvalid
            THEN
                 DECLARE
                 cvv_isvalid   BOOLEAN;
                 BEGIN
-                   cvv_isvalid := REGEXP_LIKE (CVV_Number, '^[[:digit:]]+$');
+                   cvv_isvalid := REGEXP_LIKE (CVV_Number, '^[0-9]+$');
                    IF cvv_isvalid
                        THEN
                             pId:=pay_id.nextval;
@@ -67,7 +71,7 @@ BEGIN
                     END IF;
                 END;
             ELSE
-                dbms_output.put_line('Invalid Card NUmber');
+                dbms_output.put_line(Cardnumber||' is not a valid Card Number');
         END IF;
     END;
 END;
